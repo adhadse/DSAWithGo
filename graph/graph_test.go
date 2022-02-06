ca076ba25e11c0cdaade4cfac8ddfac665947e5e
Anurag Dhadse <hello@adhadse.com>
2022-02-06 11:50:35 +0530

    added GetAdjacencyList() for a given node

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -1,6 +1,9 @@
 package graph
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdjacencyMatrix(t *testing.T) {
 	graph := MakeAdjacencyMatrix(5)
@@ -23,4 +26,23 @@ func TestAdjacencyList(t *testing.T) {
 	graph.AddEdge(3, 4)
 
 	graph.PrintAdjacencyList()
+}
+
+func TestAdjacencyList_GetAdjacencyList(t *testing.T) {
+	graph := MakeAdjacencyList(5)
+	graph.AddEdge(0, 1)
+	graph.AddEdge(0, 4)
+	graph.AddEdge(1, 2)
+	graph.AddEdge(1, 3)
+	graph.AddEdge(1, 4)
+	graph.AddEdge(2, 3)
+	graph.AddEdge(3, 4)
+
+	adjacencyList := graph.GetAdjacencyList(1)
+
+	fmt.Printf("Adjacency List of vertex %d| head", 1)
+	for _, node := range adjacencyList {
+		fmt.Printf(" -> %d", node.vertex)
+	}
+	println()
 }
