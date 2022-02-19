b93020e0cca6da39deb2b9484fbe9651608c9df5
Anurag Dhadse <anudhadse66@gmail.com>
2022-02-19 20:14:01 +0530

    Fix: kruskal's algorithm

--- graph/kruskal's_algorithm.go
+++ graph/kruskal's_algorithm.go
@@ -1,5 +1,7 @@
 package graph
 
+import "fmt"
+
 // A Minimum Spanning Tree is a subset of the edges
 // which connect all vertices in the graph with the
 // minimal total edge cost
@@ -10,10 +12,39 @@ package graph
 // 	  if the nodes are already unified/belongs to same group:
 //	  		we don't include this edge, (avoid creating cycle)
 //    else:
-// 			we include it unify the nodes
+// 			we include/unify the nodes to one group
 // 3. Terminate when every edge has been processed OR all vertices have been unified
-//func (g AdjacencyListWithWeightedNodes) KruskalMinimumSpanningTree() {
-//	var result []int
-//	i, e := 0, 0
-//
-//}
+func (g AdjacencyListWithWeightedNodes) KruskalMinimumSpanningTree() {
+	var result [][]int
+	var parent []int
+	var rank []int
+	i, e := 0, 0
+	graph := g.SortAdjacencyListBasedOnWeights()
+
+	for node := 0; node < g.numOfVertices; node++ {
+		parent = append(parent, node)
+		rank = append(rank, 0)
+	}
+
+	for e < g.numOfVertices-1 {
+		// Dammit, Find the group?
+		edge := graph[i]
+		i++
+		group1 := g.Find(parent, edge[0])
+		group2 := g.Find(parent, edge[1])
+
+		if group1 != group2 {
+			// if the nodes do not belong to same group; UNIFY THEM!!!
+			e++
+			result = append(result, []int{edge[0], edge[1], edge[2]})
+			parent, rank = g.Union(parent, rank, group1, group2)
+		}
+	}
+	minimumCost := 0
+	fmt.Println("Edges in the constructed MST")
+	for _, edge := range result {
+		minimumCost += edge[2]
+		fmt.Printf("%d -- %d == %d\n", edge[0], edge[1], edge[2])
+	}
+	fmt.Printf("Minimum Spanning Tree cost: %d", minimumCost)
+}
